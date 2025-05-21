e0c7f489df37950dd17b478a231368480b04c4f6
thread <42167023+therealthread@users.noreply.github.com>
2025-05-21 15:39:47 +0300

    1.20.4-1.20.5+ Component support

---
+++ me/anemys/customtoast/versions/ServerVersion.java
@@ -0,0 +1,27 @@
+package me.anemys.anecustomtoast.versions;
+
+public enum ServerVersion {
+    V1_20_5("1.20.5"),
+    V1_20_6("1.20.6"),
+    V1_21("1.21"),
+    V1_22("1.22"); //hope :'(
+
+    private final String version;
+
+    ServerVersion(String version) {
+        this.version = version;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public static boolean isNewVersion(String serverVersion) {
+        for (ServerVersion server : ServerVersion.values()) {
+            if (serverVersion.contains(server.getVersion())) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
