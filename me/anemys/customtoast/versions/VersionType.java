ffc0ac6869e2a52064d1a78f1261927bc77fbe28
thread <42167023+therealthread@users.noreply.github.com>
2025-06-27 19:21:51 +0300

    version format update

--- me/anemys/customtoast/versions/VersionType.java
+++ me/anemys/customtoast/versions/VersionType.java
@@ -2,6 +2,6 @@ package me.anemys.anecustomtoast.versions;
 
 public enum VersionType {
     LEGACY,   // 1.16 - 1.20.4
-    MIDDLE,   // 1.20.5 - 1.21.1
-    MODERN    // 1.21.2+
+    MIDDLE,   // 1.20.5 - 1.21.3
+    MODERN    // 1.21.4 >
 }
